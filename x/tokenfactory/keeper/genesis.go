32e430aaeefb76f7f75215b6415be54aaf2fd013
GNaD <cpt.meoz@gmail.com>
2022-09-30 17:30:56 +0700

    add genesis

--- x/tokenfactory/keeper/genesis.go
+++ x/tokenfactory/keeper/genesis.go
@@ -9,7 +9,15 @@ func (k Keeper) InitGenesis(ctx sdk.Context, genState types.GenesisState) {
 	k.SetParams(ctx, genState.Params)
 
 	for _, genDenom := range genState.GetFactoryDenoms() {
-		_, _, err := types.DeconstructDenom(genDenom.GetDenom())
+		creator, subdenom, err := types.DeconstructDenom(genDenom.GetDenom())
+		if err != nil {
+			panic(err)
+		}
+		_, err = k.CreateDenom(ctx, creator, subdenom)
+		if err != nil {
+			panic(err)
+		}
+		err = k.setAuthorityMetadata(ctx, genDenom.Denom, genDenom.AuthorityMetadata)
 		if err != nil {
 			panic(err)
 		}
@@ -18,6 +26,22 @@ func (k Keeper) InitGenesis(ctx sdk.Context, genState types.GenesisState) {
 
 func (k Keeper) ExportGenesis(ctx sdk.Context) *types.GenesisState {
 	genDenoms := []types.GenesisDenom{}
+
+	iterator := k.GetAllDenomsIterator(ctx)
+	defer iterator.Close()
+	for ; iterator.Valid(); iterator.Next() {
+		denom := string(iterator.Value())
+
+		authorityMetadata, err := k.GetAuthorityMetadata(ctx, denom)
+		if err != nil {
+			panic(err)
+		}
+
+		genDenoms = append(genDenoms, types.GenesisDenom{
+			Denom:             denom,
+			AuthorityMetadata: authorityMetadata,
+		})
+	}
 
 	return &types.GenesisState{
 		FactoryDenoms: genDenoms,
